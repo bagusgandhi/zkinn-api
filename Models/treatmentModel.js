d08fa04fdac58c6d21fdefb618bd108107de40b2
bagusgandhi <bagusgandhi4@gmail.com>
2022-05-16 21:29:59 +0700

    create schema treatmentModel

---
+++ Models/treatmentModel.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const treatmentSchema = new mongoose.Schema({
+  schedule: [],
+  users: mongoose.Types.ObjectId,
+  doctors: mongoose.Types.ObjectId,
+});
+
+module.exports = mongoose.model('user', treatmentSchema);
