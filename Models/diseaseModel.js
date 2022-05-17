8037809239796b37465dd8e206220b7962cd7716
bagusgandhi <bagusgandhi4@gmail.com>
2022-05-17 10:39:22 +0700

    fix the disease schema

--- Models/diseaseModel.js
+++ Models/diseaseModel.js
@@ -1,9 +1,14 @@
 const mongoose = require('mongoose');
 
-const diseaseSchema = new mongoose.Schema({
-  disease_name: String,
-  disease_details: Object,
-  timestamps: true,
-});
+const diseaseSchema = new mongoose.Schema(
+  {
+    disease_name: String,
+    disease_details: Object,
+    disease_img: String,
+  },
+  {
+    timestamps: true,
+  },
+);
 
 module.exports = mongoose.model('disease', diseaseSchema);
