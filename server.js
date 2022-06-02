1a1d805699110f244576e2b2a42c41e886638cdb
bagusgandhi <bagusgandhi4@gmail.com>
2022-06-02 16:00:27 +0700

    still fixing cloud run deploymen

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 // const dotenv = require('dotenv');
 
 // dotenv.config({ path: './.env' });
-// const app = require('./app');
+const app = require('./app');
 
 const DB = process.env.MONGO_URI.replace(
   '<PASSWORD>',
